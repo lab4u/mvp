80e03a78ff7920e82b48a2f44163999352df1275
Marel Oliva <marel.oliva@gmail.com>
2013-12-04 10:38:07 -0300

    Added new constants

--- Android/co.lab4u.instruments/gen/co/lab4u/instruments/R.java
+++ Android/co.lab4u.instruments/gen/co/lab4u/instruments/R.java
@@ -10,6 +10,9 @@ package co.lab4u.instruments;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int title_background=0x7f050000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,8 +20,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int flecha_roja=0x7f020000;
@@ -28,16 +31,16 @@ public final class R {
         public static final int ic_launcher=0x7f020004;
     }
     public static final class id {
-        public static final int acelerometroe=0x7f090008;
-        public static final int action_settings=0x7f090006;
-        public static final int icon=0x7f090000;
-        public static final int instrumentImageButton=0x7f090005;
-        public static final int labContent=0x7f090002;
-        public static final int labCreationDate=0x7f090003;
-        public static final int labTitle=0x7f090001;
-        public static final int labViewerZoomControls=0x7f090004;
-        public static final int search=0x7f090009;
-        public static final int spectro_cell=0x7f090007;
+        public static final int acelerometroe=0x7f0a0008;
+        public static final int action_settings=0x7f0a0006;
+        public static final int icon=0x7f0a0000;
+        public static final int instrumentImageButton=0x7f0a0005;
+        public static final int labContent=0x7f0a0002;
+        public static final int labCreationDate=0x7f0a0003;
+        public static final int labTitle=0x7f0a0001;
+        public static final int labViewerZoomControls=0x7f0a0004;
+        public static final int search=0x7f0a0009;
+        public static final int spectro_cell=0x7f0a0007;
     }
     public static final class layout {
         public static final int activity_lab_action_bar=0x7f030000;
@@ -48,31 +51,32 @@ public final class R {
         public static final int line=0x7f030005;
     }
     public static final class menu {
-        public static final int lab_action_bar=0x7f080000;
-        public static final int lab_activity_bar=0x7f080001;
-        public static final int lab_finder=0x7f080002;
-        public static final int lab_finder_item=0x7f080003;
-        public static final int lab_viewer=0x7f080004;
-        public static final int options_menu=0x7f080005;
-        public static final int searchable=0x7f080006;
+        public static final int lab_action_bar=0x7f090000;
+        public static final int lab_activity_bar=0x7f090001;
+        public static final int lab_finder=0x7f090002;
+        public static final int lab_finder_item=0x7f090003;
+        public static final int lab_viewer=0x7f090004;
+        public static final int options_menu=0x7f090005;
+        public static final int searchable=0x7f090006;
     }
     public static final class string {
-        public static final int action_settings=0x7f060001;
-        public static final int app_name=0x7f060000;
-        public static final int errorNotANumber=0x7f06000c;
-        public static final int hello_world=0x7f060002;
-        public static final int labContent=0x7f060004;
-        public static final int labTitle=0x7f060003;
-        public static final int menuShare=0x7f06000b;
-        public static final int menu_acelerometro=0x7f06000a;
-        public static final int menu_spectrocell=0x7f060009;
-        public static final int search_hint=0x7f060008;
-        public static final int search_title=0x7f060007;
-        public static final int title_activity_lab_finder_item=0x7f060005;
-        public static final int title_activity_lab_viewer=0x7f060006;
-        public static final int warningNotANumber=0x7f06000d;
+        public static final int action_settings=0x7f070001;
+        public static final int app_name=0x7f070000;
+        public static final int errorNotANumber=0x7f07000c;
+        public static final int hello_world=0x7f070002;
+        public static final int labContent=0x7f070004;
+        public static final int labTitle=0x7f070003;
+        public static final int menuShare=0x7f07000b;
+        public static final int menu_acelerometro=0x7f07000a;
+        public static final int menu_spectrocell=0x7f070009;
+        public static final int search_hint=0x7f070008;
+        public static final int search_title=0x7f070007;
+        public static final int title_activity_lab_finder_item=0x7f070005;
+        public static final int title_activity_lab_viewer=0x7f070006;
+        public static final int warningNotANumber=0x7f07000d;
     }
     public static final class style {
+        public static final int ActionBar=0x7f080003;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
@@ -86,19 +90,17 @@ public final class R {
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
     
- API 11 theme customizations can go here. 
 
         Base application theme for API 14+. This theme completely replaces
         AppBaseTheme from BOTH res/values/styles.xml and
         res/values-v11/styles.xml on API 14+ devices.
     
- API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
+        public static final int WindowTitleBackground=0x7f080002;
     }
     public static final class xml {
         public static final int searchable=0x7f040000;
